ee2a74107b176de34c325282da2372a60851e410
omeratik <fb.omeratik@hotmail.com>
2023-08-11 23:19:05 +0300

    Add project files.

---
+++ DictionaryProject/MyDictionary.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryProject
+{
+	internal class MyDictionary<TKey,TValue>
+	{
+		TKey[] keys;
+		TValue[] values;
+		TKey[] tempKeys;
+		TValue[] tempValues;
+
+		public MyDictionary()
+		{
+			values = new TValue[0];
+			keys = new TKey[0];
+		}
+		public void Add(TKey key, TValue value)
+		{
+			tempKeys = keys;
+			tempValues = values;
+
+			values=new TValue[tempKeys.Length + 1];
+			keys=new TKey[tempKeys.Length+1];
+			for (int i = 0; i < tempKeys.Length; i++)
+			{
+				keys[i] = tempKeys[i];
+				values[i] = tempValues[i];
+			}
+			values[values.Length-1] = value;
+			keys[keys.Length-1] = key;
+
+
+		}
+		
+	}
+	
+	
+}
---
+++ DictionaryProject/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+namespace DictionaryProject
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Dictionary<string,int> hastalar = new Dictionary<string, int>();
+			hastalar.Add("Kerem Canıyanık", 20);
+			hastalar.Add("Ekrem İmamoğlu", 48);
+
+			
+			
+		}
+	}
+}
